bbea819fa91846b02b624e971256f017ed76762d
m0t0-taka <tkg.8.mthr@gmail.com>
2022-08-21 16:12:13 +0900

    テスト環境設定

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
-};
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    }
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+    return compilerConfig
+  })(),
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
