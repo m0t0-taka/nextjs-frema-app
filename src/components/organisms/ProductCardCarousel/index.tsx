b7b1587c87c8ff90788041d02a77f2fa529a1a03
m0t0-taka <tkg.8.mthr@gmail.com>
2022-09-04 08:42:58 +0900

    トップページ作成

---
+++ src/components/organisms/ProductCardCarousel/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Flex from 'components/layout/Flex'
+
+interface ProductCardCarouselProps {
+  children?: React.ReactNode
+}
+
+/**
+ * 商品カードカルーセル
+ */
+const ProductCardCarousel = ({ children }: ProductCardCarouselProps) => {
+  return (
+    <Flex overflow={{ base: 'scroll', md: 'hidden' }} width="100%">
+      {children}
+    </Flex>
+  )
+}
+
+export default ProductCardCarousel
--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,7 +1,9 @@
+// 商品カテゴリ
 export type Category = 'shoes' | 'clothes' | 'book'
-
+// 商品の状態
 export type Condition = 'new' | 'used'
 
+// ユーザー
 export type User = {
   id: number
   username: string
@@ -11,18 +13,20 @@ export type User = {
   description: string
 }
 
+// 商品
 export type Product = {
   id: number
   category: Category
   title: string
   description: string
   imageUrl: string
-  blurDataUrl: number
+  blurDataUrl: string
   price: number
   condition: Condition
   owner: User
 }
 
+// APIコンテキスト
 export type ApiContext = {
   apiRootUrl: string
 }
